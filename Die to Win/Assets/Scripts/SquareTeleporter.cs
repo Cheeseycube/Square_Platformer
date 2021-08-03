984a6312baa80706c91746680d534b7f687c6fac
Cheeseycube <58313569+Cheeseycube@users.noreply.github.com>
2021-08-02 22:12:03 -0400

    worked on level 7 some more, improved the square teleporter

--- Die to Win/Assets/Scripts/SquareTeleporter.cs
+++ Die to Win/Assets/Scripts/SquareTeleporter.cs
@@ -19,6 +19,7 @@ public class SquareTeleporter : MonoBehaviour
     void Update()
     {
         CurrentLevel = SceneManager.GetActiveScene().buildIndex;
+        print(canTeleport);
 
         switch (CurrentLevel)
         {
@@ -36,14 +37,20 @@ public class SquareTeleporter : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            canTeleport = false;
+        }
+    }
+
     private void Level1()
     {
         Vector3 Playerpos = GameObject.FindGameObjectWithTag("Player").transform.position;
-        if ((Playerpos.x >= -9.5) && (Playerpos.x <= -9.2) && canTeleport && (Playerpos.y <= -2.5)) // make these variables based on level
+        if ((Playerpos.x >= -9.7) && (Playerpos.x <= -9.2) && canTeleport && (Playerpos.y <= -2.5)) // make these variables based on level was -9.5 on left
         {
-            MovePlayer.transform.position = new Vector2((float)-9.5, transform.position.y);
-            print("dslkjd");
-            canTeleport = false;
+            MovePlayer.transform.position = new Vector2((float)-9.5, Playerpos.y);
         }
 
         if (Input.GetKeyDown("h")) // fill in block after completing the square. Use a collider to check if square is completed, if not completed enable teleport again after a short delay
@@ -55,9 +62,9 @@ public class SquareTeleporter : MonoBehaviour
     private void Level7()
     {
         Vector3 Playerpos = GameObject.FindGameObjectWithTag("Player").transform.position;
-        if ((Playerpos.x >= 4008.189) && (Playerpos.x <= 4008.367) && canTeleport && (Playerpos.y <= -2.34))
+        if ((Playerpos.x >= 4008.189) && (Playerpos.x <= 4008.367) && canTeleport && (Playerpos.y <= -2.34) && (Playerpos.y >= -2.6))
         {
-            MovePlayer.transform.position = new Vector2((float)4008.29, transform.position.y);
+            MovePlayer.transform.position = new Vector2((float)4008.29, transform.position.y); // may change to playerpos.y
             canTeleport = false;
         }
         if (Input.GetKeyDown("h")) // make sure player is not below the spot when teleporting
